6ae453a619f0a2dee8729c8b15c8ef2a284adc9f
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-09-04 21:03:56 +0300

    feature

--- server.js
+++ server.js
@@ -3,16 +3,105 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/12345-Books', {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB
+
+const BooksSchema = new mongoose.Schema({
+  title:String,
+  description:String,
+  status:String
+
+});
+
+
+const Book = mongoose.model('Book', BooksSchema);
+
+async function seedData(){
+  // const GoneWithwind= new Book({ title:"Gone with Wind",
+  // description:"Scarlett O'Hara, the beautiful, spoiled daughter of a well-to-do Georgia plantation owner, must use every means at her disposal to claw her way out of the poverty she finds herself in after Sherman's March to the Sea.",
+  // status:'available'})
+
+     const HarryPotter=new Book({
+      title:"Harry Potter and the Sorcerer's Stone",
+      description:"Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs. But his fortune changes when he receives a letter that tells him the truth about himself: he's a wizard",
+      status:'Available'
+
+     })
+
+     const Mockingbird=new Book({
+      title:"To Kill a Mockingbird",
+      description:"The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. To Kill A Mockingbird, became both an instant bestseller and a critical success when it was first published in 1960.",
+      status:'Available'
+
+     })
+
+     await HarryPotter.save();
+     await Mockingbird.save();
+
+  await GoneWithwind.save();
+}
+
+
+// seedData();
+
+
+
+
 
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+
+
+app.get('/books',BooksHandler)
+
+
+
+
+
+
+
+
+function BooksHandler(req,res){
+
+
+
+   Book.find({},(err,result)=>{
+
+    if (err){
+      console.log(err)
+    }
+    else{
+      res.send(result);
+     
+    };
+
+
+
+
+
+  
+
+})
+
+}
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
