2466f95fe2e7757fc547970a3f314e555073ca66
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-09-06 01:31:51 +0300

    intaial

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(cors());
 const mongoose = require('mongoose');
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODP, {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB
+
+
+
+
+mongoose.connect(process.env.MONGODP ||process.env.MONGOCLOUD  , {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB
 
 const BooksSchema = new mongoose.Schema({
   title:String,
@@ -22,30 +26,30 @@ const BooksSchema = new mongoose.Schema({
 
 const Book = mongoose.model('Book', BooksSchema);
 
-async function seedData(){
-      const GoneWithwind= new Book({ title:"Gone with Wind",
-      description:"Scarlett O'Hara, the beautiful, spoiled daughter of a well-to-do Georgia plantation owner, must use every means at her disposal to claw her way out of the poverty she finds herself in after Sherman's March to the Sea.",
-      status:'available'})
+// async function seedData(){
+//       const GoneWithwind= new Book({ title:"Gone with Wind",
+//       description:"Scarlett O'Hara, the beautiful, spoiled daughter of a well-to-do Georgia plantation owner, must use every means at her disposal to claw her way out of the poverty she finds herself in after Sherman's March to the Sea.",
+//       status:'available'})
 
-     const HarryPotter=new Book({
-      title:"Harry Potter and the Sorcerer's Stone",
-      description:"Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs. But his fortune changes when he receives a letter that tells him the truth about himself: he's a wizard",
-      status:'Available'
+//      const HarryPotter=new Book({
+//       title:"Harry Potter and the Sorcerer's Stone",
+//       description:"Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs. But his fortune changes when he receives a letter that tells him the truth about himself: he's a wizard",
+//       status:'Available'
 
-     })
+//      })
 
-     const Mockingbird=new Book({
-      title:"To Kill a Mockingbird",
-      description:"The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. To Kill A Mockingbird, became both an instant bestseller and a critical success when it was first published in 1960.",
-      status:'Available'
+//      const Mockingbird=new Book({
+//       title:"To Kill a Mockingbird",
+//       description:"The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. To Kill A Mockingbird, became both an instant bestseller and a critical success when it was first published in 1960.",
+//       status:'Available'
 
-     })
+//      })
 
-     await HarryPotter.save();
-     await Mockingbird.save();
+//      await HarryPotter.save();
+//      await Mockingbird.save();
 
-  await GoneWithwind.save();
-}
+//   await GoneWithwind.save();
+// }
 
 
 // seedData();
